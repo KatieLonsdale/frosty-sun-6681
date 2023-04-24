b8a3696aaafcdf6eb5648fd2d505eba5d26b3256
Katie Lonsdale <katie952@gmail.com>
2023-04-24 10:53:00 -0600

    test: add patient_name test

--- spec/models/doctor_patient_spec.rb
+++ spec/models/doctor_patient_spec.rb
@@ -4,3 +4,16 @@ RSpec.describe DoctorPatient do
   it {should belong_to :doctor}
   it {should belong_to :patient}
 end
+
+describe "instance methods" do
+  before(:each) do
+    test_data
+  end
+  describe "#patient_name" do
+    it "returns the name of the patient" do
+      expect(@mcdreamy_steve.patient_name).to eq("Steve")
+      expect(@mcdreamy_tracy.patient_name).to eq("Tracy")
+      expect(@mcsteamy_granny.patient_name).to eq("Granny")
+    end
+  end
+end
