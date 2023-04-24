1573cc743b0e050d977b09638f8144ae3aea25b8
Katie Lonsdale <katie952@gmail.com>
2023-04-24 11:19:04 -0600

    test: add test for doctors_and_count

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -3,3 +3,14 @@ require 'rails_helper'
 RSpec.describe Hospital do
   it {should have_many :doctors}
 end
+
+describe "instance methods" do
+  before(:each) do
+    test_data
+  end
+  describe "#doctors_and_count" do
+    it "returns an array of doctors ordered by patient count desc" do
+      expect(@hospital_1.doctors_and_count).to eq([@doctor_1, @doctor_2, @doctor_3])
+    end
+  end
+end
