573ef8e1432ec272d932eaa1dd57ffd089093eb4
Katie Lonsdale <katie952@gmail.com>
2023-04-24 10:37:45 -0600

    test: add adult_patients test

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -4,3 +4,14 @@ RSpec.describe Patient do
   it {should have_many :doctor_patients}
   it {should have_many(:doctors).through(:doctor_patients)}
 end
+
+describe "class methods" do
+  before(:each) do
+    test_data
+  end
+  describe "adult_patients" do
+    it "returns an array of patients 18 or older alphabetized by name" do
+      expect(Patient.adult_patients).to eq([@patient_3, @patient_4, @patient_1, @patient_2])
+    end
+  end
+end
