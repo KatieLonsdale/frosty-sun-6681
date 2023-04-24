3d6e4462873ee45d785450cdd4481c27a079e3cb
Katie Lonsdale <katie952@gmail.com>
2023-04-24 11:47:07 -0600

    fix: move end

--- spec/models/doctor_patient_spec.rb
+++ spec/models/doctor_patient_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe DoctorPatient do
   it {should belong_to :doctor}
   it {should belong_to :patient}
-end
-
-describe "instance methods" do
-  before(:each) do
-    test_data
-  end
-  describe "#patient_name" do
-    it "returns the name of the patient" do
-      expect(@mcdreamy_steve.patient_name).to eq("Steve")
-      expect(@mcdreamy_tracy.patient_name).to eq("Tracy")
-      expect(@mcsteamy_granny.patient_name).to eq("Granny")
+  
+  describe "instance methods" do
+    before(:each) do
+      test_data
+    end
+    describe "#patient_name" do
+      it "returns the name of the patient" do
+        expect(@mcdreamy_steve.patient_name).to eq("Steve")
+        expect(@mcdreamy_tracy.patient_name).to eq("Tracy")
+        expect(@mcsteamy_granny.patient_name).to eq("Granny")
+      end
     end
   end
 end
