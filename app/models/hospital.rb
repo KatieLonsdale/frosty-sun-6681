52d7ed2efac1770f800d3fd71cb1c718bf15d8ec
Katie Lonsdale <katie952@gmail.com>
2023-04-24 11:19:16 -0600

    feat: add doctors_and_count method

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,10 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doctors_and_count
+    doctors.joins(:patients)
+           .select("doctors.*, COUNT(patients.id) AS patient_count")
+           .order("patient_count DESC")
+           .group(:id)
+  end
 end
