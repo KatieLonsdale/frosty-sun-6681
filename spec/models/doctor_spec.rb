52975c22d6f1251bc807f6d17afb27764ee0ed1c
Katie Lonsdale <katie952@gmail.com>
2023-04-24 11:47:42 -0600

    fix: move end

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe Doctor do
   it {should belong_to :hospital}
   it {should have_many :doctor_patients}
   it {should have_many(:patients).through(:doctor_patients)}
-end
-
-describe "instance methods" do
-  before(:each) do
-    test_data
-  end
-  describe "#hospital_name" do
-    it "returns the name of the hospital they work at" do
-      expect(@doctor_1.hospital_name).to eq("Seattle Grace")
-      expect(@doctor_2.hospital_name).to eq("Seattle Grace")
+  
+  describe "instance methods" do
+    before(:each) do
+      test_data
+    end
+    describe "#hospital_name" do
+      it "returns the name of the hospital they work at" do
+        expect(@doctor_1.hospital_name).to eq("Seattle Grace")
+        expect(@doctor_2.hospital_name).to eq("Seattle Grace")
+      end
     end
   end
 end
